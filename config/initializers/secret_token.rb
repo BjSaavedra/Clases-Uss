88c3f14ca3b37a5a61a8fff47e731a05561555cc
BjSaavedra <bsaavedra@live.cl>
2014-04-12 20:54:31 -0300

    firts commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Clases::Application.config.secret_key_base = '4f30a4a8c3c63b5955463cbdae66ebcc02f17562798829ba5f0fed48f0fa86e39043289551c985c4b2e4fd2b79e67e70ea5fe5ce9c207312b1099300903e4019'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Clases::Application.config.session_store :cookie_store, key: '_clases_session'
